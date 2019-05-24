382fb52d78e32dfd593fdb7b8c5116a0c81ef8bd
Merhawi Ghebre Selassie <merhawi.gs@gmail.com>
2019-05-24 14:09:46 +0200

    commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,34 +10,6 @@ const IndexPage = () => {
     color: "#fff",
   }
 
-  const scroll = e => {
-    //console.log(e)
-    console.log(window.scrollY)
-  }
-  const onScrollSpy = () => {
-    var section = document.querySelectorAll(".section")
-    var sections = {}
-    var i = 0
-
-    Array.prototype.forEach.call(section, function(e) {
-      sections[e.id] = e.offsetTop
-    })
-
-    window.onscroll = function() {
-      var scrollPosition =
-        document.documentElement.scrollTop || document.body.scrollTop
-
-      for (i in sections) {
-        if (sections[i] <= scrollPosition) {
-          document.querySelector(".active").setAttribute("class", " ")
-          document
-            .querySelector("a[href*=" + i + "]")
-            .setAttribute("class", "active")
-        }
-      }
-    }
-  }
-  onScrollSpy()
   return (
     <div id="scroll" onScroll={scroll}>
       <Head title="Home" />
