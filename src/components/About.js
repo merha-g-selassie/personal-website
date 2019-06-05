8a33a10cc1fb08af6e0c851679aedcf7343e942d
Merhawi Ghebre Selassie <merhawi.gs@gmail.com>
2019-06-05 17:59:06 +0200

    commit

--- src/components/About.js
+++ src/components/About.js
@@ -1,65 +1,94 @@
 import React from "react"
+import atom from "../images/atom.svg"
+import astronaut from "../images/atronaut.svg"
+import milleniumFalcon from "../images/Millennium-Falcon.svg"
+import telescope from "../images/telescope.svg"
 import AboutStyles from "./About.module.scss"
 
 const About = ({ height, id }) => {
   return (
     <div>
       <div style={height} id={id} className={AboutStyles.flex}>
-        <h2>About</h2>
+        <h2 className={AboutStyles.headerMargin}>About</h2>
         {/*frontend, backend, tooling*/}
         <div className={AboutStyles.alignStart}>
           <div className={AboutStyles.skills}>
             <h3>Frontend</h3>
-            <ul>
-              <li>HTML</li>
-              <li>CSS</li>
-              <li>SASS</li>
-              <li>Javascript</li>
-              <li>JQuery</li>
-              <li>React</li>
-              <li>Redux</li>
-              <li>Electron</li>
-              <li>Android</li>
-              <li>Swift</li>
-            </ul>
+            <div className={AboutStyles.skillImgFlex}>
+              <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>SASS</li>
+                <li>Javascript</li>
+                <li>JQuery</li>
+                <li>React</li>
+                <li>Redux</li>
+                <li>Electron</li>
+                <li>Android</li>
+                <li>Swift</li>
+              </ul>
+              <div>
+                <img src={atom} alt="atom" width="200px" />
+              </div>
+            </div>
           </div>
         </div>
         <div className={AboutStyles.alignEnd}>
-          <div>
+          <div className={AboutStyles.skills}>
             <h3>Backend</h3>
-            <ul>
-              <li>Node</li>
-              <li>Express</li>
-              <li>Java</li>
-              <li>Spring Boot</li>
-              <li>PHP</li>
-              <li>Laravel</li>
-              <li>SQL</li>
-              <li>PL/SQL</li>
-            </ul>
+            <div className={AboutStyles.skillImgFlex}>
+              <div>
+                <img
+                  src={milleniumFalcon}
+                  alt="milleniumFalcon"
+                  width="200px"
+                />
+              </div>
+              <ul>
+                <li>Node</li>
+                <li>Express</li>
+                <li>Java</li>
+                <li>Spring Boot</li>
+                <li>PHP</li>
+                <li>Laravel</li>
+                <li>SQL</li>
+                <li>PL/SQL</li>
+              </ul>
+            </div>
           </div>
         </div>
         <div className={AboutStyles.alignStart}>
-          <div>
+          <div className={AboutStyles.skills}>
             <h3>Databases</h3>
-            <ul>
-              <li>Oracle</li>
-              <li>PostgreSQL</li>
-              <li>MySQL</li>
-              <li>MongoDB</li>
-              <li>Redis</li>
-              <li>Cloud Firestore</li>
-            </ul>
+            <div className={AboutStyles.skillImgFlex}>
+              <ul>
+                <li>Oracle</li>
+                <li>PostgreSQL</li>
+                <li>MySQL</li>
+                <li>MongoDB</li>
+                <li>Redis</li>
+                <li>Cloud Firestore</li>
+              </ul>
+              <div>
+                <img src={astronaut} alt="astronaut" width="200px" />
+              </div>
+            </div>
           </div>
         </div>
         <div className={AboutStyles.alignEnd}>
-          <div>
+          <div className={AboutStyles.skills}>
             <h3>Others</h3>
-            <ul>
-              <li>Git</li>
-              <li>Webpack</li>
-              <li>Scrum</li>
-            </ul>
+            <div className={AboutStyles.skillImgFlex}>
+              <div>
+                <img src={telescope} alt="telescope" width="200px" />
+              </div>
+              <ul>
+                <li>Git</li>
+                <li>Webpack</li>
+                <li>Docker</li>
+                <li>Scrum</li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
