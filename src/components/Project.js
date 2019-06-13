9be94d14de04fbda7b2a6297d3881d6a160e6604
Merhawi Ghebre Selassie <merhawi.gs@gmail.com>
2019-06-13 12:07:16 +0200

    commit

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,14 +1,25 @@
-import React from "react"
+import React, { useState } from "react"
 import yelpcamp from "../images/yelpcamp.png"
 import ProjectStyle from "./Project.module.scss"
 
 const Project = ({ projectName }) => {
+  const [show, setShow] = useState(false)
+
+  const Modal = <div>hello</div>
+
+  const showModal = () => {
+    setShow(!show)
+    console.log(show)
+  }
+
   return (
     <div className={ProjectStyle.imgWrap}>
       <img className={ProjectStyle.imgImg} src={yelpcamp} alt="project" />
       <div className={ProjectStyle.imgDescription}>
         <h3>{projectName}</h3>
-        <button className={ProjectStyle.button}>See more</button>
+        <button onClick={showModal} className={ProjectStyle.button}>
+          See more
+        </button>
       </div>
     </div>
   )
